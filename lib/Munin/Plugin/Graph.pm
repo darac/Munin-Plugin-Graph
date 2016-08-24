625300a5d303479d9e996883977ac6eb743e559e
Paul Saunders <darac@darac.org.uk>
2016-08-24 23:01:07 +0100

    Bump version. Welcome to 0.04!

--- lib/Munin/Plugin/Graph.pm
+++ lib/Munin/Plugin/Graph.pm
@@ -14,11 +14,11 @@ Munin::Plugin::Graph - The great new Munin::Plugin::Graph!
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
